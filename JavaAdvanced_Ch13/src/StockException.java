6d66f37988c41f63d1386b28d43c2de308b49fc2
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-09-30 23:09:54 +0200

    JavaAdvanced Ch13

---
+++ JavaAdvanced_Ch13/src/StockException.java
@@ -0,0 +1,6 @@
+public class StockException extends Exception {
+    private static final long serialVersionUID = 103L;
+    public StockException (String reason) {
+        super (reason);
+    }
+}
