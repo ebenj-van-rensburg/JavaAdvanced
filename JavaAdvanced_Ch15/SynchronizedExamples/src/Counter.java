e930f3c3030a998b6f94692b4ee551dc9dc3e27c
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-10-08 20:33:38 +0200

    JavaAdvanced Ch 15

---
+++ JavaAdvanced_Ch15/SynchronizedExamples/src/Counter.java
@@ -0,0 +1,15 @@
+public class Counter {
+  private static int i = 0;
+  
+  public void increment(){
+    i++;
+  }
+  
+  public void decrement(){
+    i--;
+  }
+  
+  public int getValue(){
+    return i;
+  }
+}
