b97c157ff36eb6f2ea1d6d00518b6463e525ee19
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-10-15 17:12:57 +0200

    13 practice

--- JavaAdvanced_Ch13/src/ByteChannelCopyTest.java
+++ JavaAdvanced_Ch13/src/ByteChannelCopyTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,10 +5,6 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
-/**
- *
- * @author tmcginn
- */
 public class ByteChannelCopyTest {
 
     public static void main(String[] args) {
